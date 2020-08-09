569aa306f3ede4064a535d179c9205bba6b28536
reflective-refactoring-7309 <bristuttii@gmail.com>
2020-08-09 18:32:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,7 @@
  fruits = {"apples" => 10, "oranges" => 3, "bananas"=> 8}
 
  def key_for_min_value(fruits)
-  
-
-  
-  lowest_amount = 10
+   lowest_amount = 10
   lowest_fruit = nil
   
   fruits.each do |fruit,amount|
@@ -21,26 +18,25 @@ lowest_fruit
 end 
 
 
-
-#def key_for_min_value(name_hash)
- # lowest_value = 0
-  #lowest_key = nil
+def key_for_min_value(name_hash)
+ lowest_value = 0
+  lowest_key = nil
   
-  # name_hash.each do |key,value|
+   name_hash.each do |key,value|
    
-    #if the lowest_value is in it's intial state  
-    #or if the value of the current key is less than the lowest_value
-    #then: set the lowest_value to value and lowest_key to key
+   # if the lowest_value is in it's intial state  
+   # or if the value of the current key is less than the # lowest_value
+    # then: set the lowest_value to value and lowest_key to key
    
-   # if lowest_value == 0 || value < lowest_value
-    #  lowest_value = value 
-     # lowest_key = key
-   # end
- # end
-#  lowest_key
-# end
+    if lowest_value == 0 || value < lowest_value
+    lowest_value = value 
+     lowest_key = key
+    end
+  end
+lowest_key
+end
 
-# ikea = {:chair => 25, :table => 85, :mattress => 450}
-# key_for_min_value(ikea)
+ ikea = {:chair => 25, :table => 85, :mattress => 450}
+ key_for_min_value(ikea)
 
 
